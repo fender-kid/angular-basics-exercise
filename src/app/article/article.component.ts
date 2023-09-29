01c3625a523801c272c41aa98502064ed36a7998
fender-kid <jtt.sides@gmail.com>
2023-09-28 20:08:17 -0500

    setup basic project and added an article component

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css'],
+})
+
+export class ArticleComponent {
+  title: string = "My Default Title"
+  content: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  isTechRelated: boolean = true
+}
+
